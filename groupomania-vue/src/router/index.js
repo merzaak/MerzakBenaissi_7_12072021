5db7924b3333ba07c98e58f5b19dec4489643568
Merzak Benaissi <merzak89merzak@gmail.com>
2021-07-13 13:36:48 +0200

    ajout des routes Login et Profile

--- groupomania-vue/src/router/index.js
+++ groupomania-vue/src/router/index.js
@@ -1,19 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
